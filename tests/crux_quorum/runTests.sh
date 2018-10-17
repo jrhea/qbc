7bca16bec9a8f80fb46e041b5fcaf3679b3546af
Jonny Rhea <jonathan.rhea@gmail.com>
2018-10-16 20:23:43 -0500

    wrote some tests to make sure that the crux-quorum install works (#10)
    * wrote some tests to make sure that the crux-quorum install works.  
    
    * added a 10 min timeout to the waitForStart script logic
    
    * added logic to wait for all peers to see each other before waiting for the blockNo to increment. both loops have 10 min timeouts

---
+++ tests/crux_quorum/runTests.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+INDEX=-1
+PASSED=()
+
+echo "test private tx: is readable by recipient?"
+INDEX=$INDEX+1
+PASSED+=(false)
+message="0xca843569e3427144cead5e4d5999a3d0ccf92b8eed9d02e382b34818e88b88a309c7fe71e65f419d"
+echo "  sending message from node1 to node2"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0xed9d02e382b34818e88b88a309c7fe71e65f419d", "to": "0xca843569e3427144cead5e4d5999a3d0ccf92b8e", "gas": "0x76c0", "data": "'$message'", "privateFor": ["QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc="]}],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  transaction hash: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["'$result'"],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result.input);"`
+echo "  transaction input: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0", "method":"eth_getQuorumPayload", "params":["'$result'"], "id":1}' 0.0.0.0:22002`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  message(from node2's POV): $result"
+if [ "$result" == "$message" ]; then
+    echo "PASSED"
+    PASSED[$INDEX]=true
+else
+    echo "FAILED"
+fi
+
+echo "test private tx: is not readable by others?"
+INDEX=$INDEX+1
+PASSED+=(false)
+message="0xca843569e3427144cead5e4d5999a3d0ccf92b8eed9d02e382b34818e88b88a309c7fe71e65f419d"
+echo "  sending message from node1 to node2"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0xed9d02e382b34818e88b88a309c7fe71e65f419d", "to": "0xca843569e3427144cead5e4d5999a3d0ccf92b8e", "gas": "0x76c0", "data": "'$message'", "privateFor": ["QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc="]}],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  transaction hash: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["'$result'"],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result.input);"`
+echo "  transaction input: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0", "method":"eth_getQuorumPayload", "params":["'$result'"], "id":1}' 0.0.0.0:22003`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  message(from node3's POV): $result"
+if [ "$result" == "0x" ]; then
+    echo "PASSED"
+    PASSED[$INDEX]=true
+else
+    echo "FAILED"
+fi
+
+echo "test public tx: is readable by recipient?"
+INDEX=$INDEX+1
+PASSED+=(false)
+message="0xca843569e3427144cead5e4d5999a3d0ccf92b8eed9d02e382b34818e88b88a309c7fe71e65f419d"
+echo "  sending message from node1 to node3"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0xed9d02e382b34818e88b88a309c7fe71e65f419d", "to": "0x0fbdc686b912d7722dc86510934589e0aaf3b55a", "gas": "0x76c0", "data": "'$message'"}],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  transaction hash: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["'$result'"],"id":1}' 0.0.0.0:22002`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result.input);"`
+echo "  message(from node2's POV): $result"
+
+if [ "$result" == "$message" ]; then
+    echo "PASSED"
+    PASSED[$INDEX]=true
+else
+    echo "FAILED"
+fi
+
+echo "test public tx: is readable by others?"
+INDEX=$INDEX+1
+PASSED+=(false)
+message="0xca843569e3427144cead5e4d5999a3d0ccf92b8eed9d02e382b34818e88b88a309c7fe71e65f419d"
+echo "  sending message from node1 to node3"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0xed9d02e382b34818e88b88a309c7fe71e65f419d", "to": "0x0fbdc686b912d7722dc86510934589e0aaf3b55a", "gas": "0x76c0", "data": "'$message'"}],"id":1}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+echo "  transaction hash: $result"
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["'$result'"],"id":1}' 0.0.0.0:22003`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result.input);"`
+echo "  message(from node3's POV): $result"
+
+if [ "$result" == "$message" ]; then
+    echo "PASSED"
+    PASSED[$INDEX]=true
+else
+    echo "FAILED"
+fi
+
+for PASSED in ${PASSED[@]}; do
+  if "$PASSED" = true; then exit 0; else exit 1; fi
+done
---
+++ tests/crux_quorum/waitForStart.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Wait for each node to have 3 peers
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":74}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+counter=0
+end=$((counter+600))
+nodeNum=0;
+while [ $nodeNum -lt 4 ]; do
+    if [ $counter -gt $end ]; then
+        break
+    else
+        echo "waiting for nodes to connect"
+        sleep 1
+        json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":74}' 0.0.0.0:$((22001+nodeNum))`
+        result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+        if [ "$result" == "0x3" ]; then
+          let nodeNum=$nodeNum+1
+        fi
+        echo "node: $nodeNum numPeers: $result"
+    fi
+    let counter=$counter+1
+
+done;
+
+# Wait for the blockNo to start incrementing
+json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":74}' 0.0.0.0:22001`
+result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+counter=0
+end=$((counter+600))
+while [ "$result" == "0x0" ]; do
+
+    if [ $counter -gt $end ]; then
+        break
+    else
+        echo "waiting for nodes to initialize"
+        sleep 1
+        json=`curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":74}' 0.0.0.0:22001`
+        result=`node -e "obj = JSON.parse(JSON.stringify($json)); console.log(obj.result);"`
+        echo "blockNumber: $result"
+    fi
+    let counter=$counter+1
+done;
