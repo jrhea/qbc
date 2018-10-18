8ff51cbb739b03e01cb3399a3be2b5fbd554a6bd
Antoine Toulme <atoulme@users.noreply.github.com>
2018-10-18 06:03:08 -0700

    Add network interface argument to bind crux to 0.0.0.0 in docker (#21)

--- docker/crux-start.sh
+++ docker/crux-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/crux --url=http://$HOSTNAME:9000/ --port=9000 --grpcport=9001 --workdir=/var/cdata/ --socket=../qdata/tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=$OTHER_NODES --verbosity 4 >> /var/cdata/logs/crux.log 2>&1
+/opt/crux --url=http://$HOSTNAME:9000/ --networkinterface=0.0.0.0 --port=9000 --grpcport=9001 --workdir=/var/cdata/ --socket=../qdata/tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=$OTHER_NODES --verbosity 4 >> /var/cdata/logs/crux.log 2>&1
