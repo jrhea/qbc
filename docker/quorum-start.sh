fca5688d3ae7cd358f12a703fa34a2c0f57ab4df
jonathan rhea <jonathan.rhea@gmail.com>
2018-10-19 14:00:16 -0500

    add tests to constellation and modified setup to make them work

--- docker/quorum-start.sh
+++ docker/quorum-start.sh
@@ -5,5 +5,6 @@ while [ ! -S "/var/qdata/tm.ipc" ]; do
    sleep 0.1
 done
 
-ARGS="--syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul --nodiscover"
-/opt/geth --datadir /var/qdata/dd $ARGS --rpcport 22000 --port 21000 --nodekey /var/qdata/nodekey --unlock 0 --password /var/qdata/passwords.txt --verbosity=6 2>>/var/qdata/logs/node.log
+TMCONF=/var/qdata/tm.conf
+GETH_ARGS="--syncmode full --mine --port 21000 --rpc --rpcport 22000 --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul --nodiscover --datadir /var/qdata/dd --nodekey /var/qdata/nodekey --unlock 0 --password /var/qdata/passwords.txt"
+PRIVATE_CONFIG=$TMCONF /opt/geth $GETH_ARGS --verbosity=6 2>>/var/qdata/logs/node.log
